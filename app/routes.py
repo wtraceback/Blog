08b9bbda16a2539bca81f4c307323b21b4acdafb
whxcer <whxcer@outlook.com>
2020-10-02 16:34:45 +0800

    使用 flask-wtf 制作登录表单

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,17 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    title = "Personal-Blog"
+    return render_template('index.html', title='Personal-Blog')
 
-    return render_template('index.html', title=title)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print('test')
+        return redirect('/index')
+    return render_template('login.html', title='Log in', form=form)
