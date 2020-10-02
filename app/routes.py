2e7dac0936906a305c136ce486d4f63420258ee1
whxcer <whxcer@outlook.com>
2020-10-02 22:04:35 +0800

    密码哈希和用户认证

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from app import app
 from flask import render_template, redirect, flash, url_for
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from flask_login import login_required
+from app.models import Admin
+
 
 @app.route('/')
 @app.route('/index')
@@ -10,8 +14,24 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
     form = LoginForm()
     if form.validate_on_submit():
+        user = Admin.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+        login_user(user, remember=form.remember_me.data)
         flash('Login successful')
         return redirect(url_for('index'))
     return render_template('login.html', title='Log in', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
