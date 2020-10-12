69877302e115f13720f784b74909fae236e42b42
whxcer <whxcer@outlook.com>
2020-10-12 22:24:24 +0800

    使用 flask-bootstrap 来美化界面

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,5 +12,6 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 login.login_message = 'Please log in'
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
