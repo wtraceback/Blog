170eb07f5da1dcc150a3db7bacc07246dafa738a
whxcer <whxcer@outlook.com>
2020-10-16 22:24:47 +0800

    使用 Flask-Moment本地化日期和时间

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,5 +14,6 @@ login = LoginManager(app)
 login.login_view = 'login'
 login.login_message = 'Please log in'
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes, models, errors
